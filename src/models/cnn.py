d97ea6c0b971d0f99cbcc647296ac265697a5f24
Delshi <redstupidyt@outlook.com>
2025-11-20 22:08:15 +0300

    89% acc but learning MUCH faster (like twice)

--- src/models/cnn.py
+++ src/models/cnn.py
@@ -2,14 +2,12 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from .residual import ResidualBlock
-from .bottleneck import BottleneckBlock
 
 
 class CNN(nn.Module):
-    def __init__(self, num_classes=6, use_bottleneck=False, dropout_rate=0.3):
+    def __init__(self, num_classes=6, dropout_rate=0.3):
         super(CNN, self).__init__()
 
-        self.use_bottleneck = use_bottleneck
         self.in_channels = 64
 
         self.conv1 = nn.Conv2d(
@@ -29,31 +27,10 @@ class CNN(nn.Module):
         )
         self.dropout = nn.Dropout(0.1)
 
-        # if use_bottleneck:
-        #     self.layer1 = self._make_bottleneck_layer(64, 64, blocks=2, stride=1)
-        #     self.layer2 = self._make_bottleneck_layer(
-        #         256, 128, blocks=2, stride=2
-        #     )  # 64*4=256
-        #     self.layer3 = self._make_bottleneck_layer(
-        #         512, 256, blocks=2, stride=2
-        #     )  # 128*4=512
-        #     self.layer4 = self._make_bottleneck_layer(
-        #         1024, 512, blocks=2, stride=2
-        #     )  # 256*4=1024
-
-        #     self.classifier = nn.Sequential(
-        #         nn.Dropout(dropout_rate),
-        #         nn.Linear(2048, 1024),
-        #         nn.ReLU(inplace=True),
-        #         nn.Dropout(dropout_rate / 2),
-        #         nn.Linear(1024, num_classes),
-        #     )
-        # else:
         self.layer1 = self._make_layer(64, 64, blocks=2, stride=1)
         self.layer2 = self._make_layer(64, 128, blocks=2, stride=2)
         self.layer3 = self._make_layer(128, 256, blocks=2, stride=2)
         self.layer4 = self._make_layer(256, 512, blocks=2, stride=2)
-        #!
         self.layer5 = self._make_layer(512, 1024, blocks=2, stride=2)
 
         self.classifier = nn.Sequential(
@@ -64,8 +41,16 @@ class CNN(nn.Module):
             nn.Dropout(dropout_rate / 1.75),
             nn.Linear(2048, num_classes),
         )
-        # end of else
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+
+        self.bn2 = nn.BatchNorm2d(512)
+
+        # Добавляем downsample слои для приведения размерностей
+        self.downsample_x3_to_x4 = self._create_downsample(256, 512, stride=2)
+        self.downsample_x3_to_x5 = self._create_downsample(
+            256, 1024, stride=4
+        )  # layer4 + layer5 оба с stride=2
+        self.downsample_x4_to_x5 = self._create_downsample(512, 1024, stride=2)
 
     def _make_layer(self, in_channels, out_channels, blocks, stride):
         """Для обычных Residual блоков"""
@@ -91,30 +76,37 @@ class CNN(nn.Module):
 
         return nn.Sequential(*layers)
 
-    # def _make_bottleneck_layer(self, in_channels, out_channels, blocks, stride):
-    #     """Для Bottleneck блоков"""
-    #     downsample = None
-    #     expansion = 4
+    # def forward(self, x):
+    #     x = self.conv1(x)
+    #     x = self.bn1(x)
+    #     x = self.relu(x)
+    #     x = self.maxpool(x)
 
-    #     if stride != 1 or in_channels != out_channels * expansion:
-    #         downsample = nn.Sequential(
-    #             nn.Conv2d(
-    #                 in_channels,
-    #                 out_channels * expansion,
-    #                 kernel_size=1,
-    #                 stride=stride,
-    #                 bias=False,
-    #             ),
-    #             nn.BatchNorm2d(out_channels * expansion),
-    #         )
+    #     x = self.layer1(x)
+    #     x = self.layer2(x)
+    #     x = self.layer3(x)
+    #     x = self.layer4(x)
+    #     x = self.layer5(x)
+    #     x = self.dropout(x)
 
-    #     layers = []
-    #     layers.append(BottleneckBlock(in_channels, out_channels, stride, downsample))
+    #     x = self.avgpool(x)
+    #     x = x.view(x.size(0), -1)
+    #     x = self.classifier(x)
 
-    #     for _ in range(1, blocks):
-    #         layers.append(BottleneckBlock(out_channels * expansion, out_channels))
+    #     return x
 
-    #     return nn.Sequential(*layers)
+    def _create_downsample(self, in_channels, out_channels, stride):
+        """Создает downsample слой для приведения размерностей"""
+        return nn.Sequential(
+            nn.Conv2d(
+                in_channels,
+                out_channels,
+                kernel_size=1,
+                stride=stride,
+                bias=False,
+            ),
+            nn.BatchNorm2d(out_channels),
+        )
 
     def forward(self, x):
         x = self.conv1(x)
@@ -122,15 +114,29 @@ class CNN(nn.Module):
         x = self.relu(x)
         x = self.maxpool(x)
 
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        x = self.layer4(x)
-        x = self.layer5(x)
-        x = self.dropout(x)
+        x1 = self.layer1(x)  # (1)
+        x2 = self.layer2(x1)  # (2)
+        x3 = self.layer3(x2)  # (3)
+
+        # (4) = (4 полученный из 3) + (3)
+        x4_output = self.layer4(x3)
+        x3_downsampled = self.downsample_x3_to_x4(x3)
+        x4 = x4_output + x3_downsampled
+        x4 = self.bn2(x4)
+
+        # (5) = (5 полученный из 4) + (3) + (4)
+        x5_output = self.layer5(x4)
+        x3_downsampled_to_x5 = self.downsample_x3_to_x5(x3)
+        x4_downsampled_to_x5 = self.downsample_x4_to_x5(x4)
+        x5 = x5_output + x3_downsampled_to_x5 + x4_downsampled_to_x5
+
+        x = self.dropout(x5)
 
         x = self.avgpool(x)
         x = x.view(x.size(0), -1)
+
+        x = self.dropout(x)
+
         x = self.classifier(x)
 
         return x
